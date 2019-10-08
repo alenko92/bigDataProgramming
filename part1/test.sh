33e3cc141e5029953925100e54f180b3eaa066bc
Alex <47699646+alenko92@users.noreply.github.com>
2019-10-08 00:54:55 -0400

    Update test.sh

--- part1/test.sh
+++ part1/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-../start.sh
+../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
@@ -11,4 +11,4 @@
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
-../stop.sh
+../../stop.sh

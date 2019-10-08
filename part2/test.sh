636ad72b18979a2042c4379551a5dd1de8e972a5
Alex <47699646+alenko92@users.noreply.github.com>
2019-10-08 00:41:24 -0400

    Update test.sh

--- part2/test.sh
+++ part2/test.sh
@@ -5,8 +5,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../bigDataProgramming/part1/mapper.py -mapper ../../bigDataProgramming/part1/mapper.py \
--file ../../bigDataProgramming/part1/reducer.py -reducer ../../bigDataProgramming/part1/reducer.py \
+-file ../../bigDataProgramming/part2/mapper.py -mapper ../../bigDataProgramming/part2/mapper.py \
+-file ../../bigDataProgramming/part2/reducer.py -reducer ../../bigDataProgramming/part2/reducer.py \
 -input /logstat2/input/* -output /logstat2/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/

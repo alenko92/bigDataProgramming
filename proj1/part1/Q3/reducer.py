ba4cce563428ed63c750e5b33366f1e1f99dd39b
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-11 20:51:38 -0500

    Add files via upload

---
+++ proj1/part1/Q3/reducer.py
@@ -0,0 +1,19 @@
+import sys
+
+
+count = {}
+
+for line in sys.stdin:
+    line = line.strip()
+    location, number = line.split('\t')
+    try:
+        number = int()
+        count[location] = count.get(location, 0) + number
+
+    except ValueError:
+        pass
+
+sorted_count = sorted(count.items(), key=lambda x:-x[1])
+for location, count in sorted_count[0:5]:
+    # print('%s\t%s' % (location, count))
+    print(location, '->', count)
---
+++ proj1/part1/Q3/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+../../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /temp/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../data/Parking_Violations_Issued_-_Fiscal_Year_2014.csv /temp/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-file ../q3/mapper.py -mapper ../q3/mapper.py \
+-file ../q3/reducer.py -reducer ../q3/reducer.py \
+-input /temp/input/* -output /temp/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /temp/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
+../../../stop.sh

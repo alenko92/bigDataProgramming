f010e7e6408e7e98cb2066df729674461a702ab6
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-11 20:49:46 -0500

    Add files via upload

---
+++ proj1/part1/Q2/reducer.py
@@ -0,0 +1,19 @@
+import sys
+
+
+count = {}
+
+for line in sys.stdin:
+    line = line.strip()
+    year_type, number = line.split('\t')
+    try:
+        number = int()
+        count[year_type] = count.get(year_type, 0) + number
+
+    except ValueError:
+        pass
+
+sorted_count = sorted(count.items(), key = lambda x:-x[1])
+for car, count in sorted_count[0:5]:
+    # print('%s\t%s' % (car, count))
+    print(car, '-', count)

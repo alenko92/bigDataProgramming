a2a62438d94b4a55f41266aeb8d6e73dda0c4d28
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-11 20:46:48 -0500

    Add files via upload

---
+++ proj1/part1/Q1/reducer.py
@@ -0,0 +1,20 @@
+import sys
+    
+
+time_count = {}
+
+for line in sys.stdin:
+    line = line.strip()
+    time, number = line.split('\t')
+
+    try:
+        number = int()
+        time_count[time] = time_count.get(time, 0) + number
+
+    except ValueError:
+        pass
+
+sorted_time_count = sorted(time_count.items(), key = lambda x:-x[1])
+for time, count in sorted_time_count[0:5]:
+    # print('%s\t%s' % (time, count))
+    print(time, '\t', count)

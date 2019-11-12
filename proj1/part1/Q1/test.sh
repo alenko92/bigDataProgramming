3d6ad5d6e1acfd4d27a03dc598dc291c1b9705e7
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-11 21:28:42 -0500

    Update test.sh

--- proj1/part1/Q1/test.sh
+++ proj1/part1/Q1/test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export PATH=$PATH:/usr/local/hadoop/bin/
-../../../start.sh
+../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /temp/input/
@@ -12,4 +11,4 @@ export PATH=$PATH:/usr/local/hadoop/bin/
 /usr/local/hadoop/bin/hdfs dfs -cat /temp/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
-../../../stop.sh
+../../stop.sh

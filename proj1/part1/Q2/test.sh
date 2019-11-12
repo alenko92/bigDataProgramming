1cb1948320e8941400dec719eeec28de9c5a3da9
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-12 15:01:35 -0500

    Update test.sh

--- proj1/part1/Q2/test.sh
+++ proj1/part1/Q2/test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-../../../start.sh
+../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /temp/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../data/Parking_Violations_Issued_-_Fiscal_Year_2014.csv /temp/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../Parking_Violations_Issued_-_Fiscal_Year_2014.csv /temp/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../q2/mapper.py -mapper ../q2/mapper.py \
--file ../q2/reducer.py -reducer ../q2/reducer.py \
+-files <../Q2/mapper.py> -mapper ../Q2/mapper.py \
+-files <../Q2/reducer.py> -reducer ../Q2/reducer.py \
 -input /temp/input/* -output /temp/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /temp/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
-../../../stop.sh
+../../stop.sh

0aff35bb8d63af80963f780cdf352763ce822612
Alex <47699646+alenko92@users.noreply.github.com>
2019-11-15 18:02:53 -0500

    Update test.sh

--- proj1/part1/Q1/test.sh
+++ proj1/part1/Q1/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /temp/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /temp/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../Parking_Violations_Issued_-_Fiscal_Year_2014.csv /temp/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../Parking_Violations_Issued_-_Fiscal_Year_2020.csv /temp/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -files <../Q1/mapper.py> -mapper ../Q1/mapper.py \
 -files <../Q1/reducer.py> -reducer ../Q1/reducer.py \

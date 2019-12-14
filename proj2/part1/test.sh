c3f80bd34194d76c4f4d92a3aee3c116f2108372
Alex <47699646+alenko92@users.noreply.github.com>
2019-12-13 23:32:32 -0500

    Update test.sh

--- proj2/part1/test.sh
+++ proj2/part1/test.sh
@@ -2,6 +2,6 @@
 source ../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /proj2/part1/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /proj2/part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/train.csv /proj2/part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/test.csv /proj2/part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../train.csv /proj2/part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../test.csv /proj2/part1/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./part1.py hdfs://$SPARK_MASTER:9000/proj2/part1/input/

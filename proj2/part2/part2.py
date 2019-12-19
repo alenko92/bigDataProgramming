b752597b1b44c4c57b549a7358c8982c346c3511
Alex <47699646+alenko92@users.noreply.github.com>
2019-12-19 18:29:30 -0500

    Update part2.py

--- proj2/part2/part2.py
+++ proj2/part2/part2.py
@@ -1,185 +1,145 @@
 # Proj2
 # Part2: Heart Disease Prediction using Logistic Regression
+# Alexey Sanko
 #
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-#import os
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,when,udf
 from pyspark.sql import types as T
 from pyspark.ml.linalg import Vectors, VectorUDT, SparseVector, DenseVector
 from pyspark.ml import Pipeline
-#Import the vector assembler
 from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
-#Scalarize the newly-created column 'feature'
 from pyspark.ml.feature import StandardScaler
-# Feature selection using chisquareSelector
 from pyspark.ml.feature import ChiSqSelector
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.evaluate import BinaryClassificationEvaluator
 
-# converts sparse vector to dense vector
-def sparse_to_array(v):
-    v = DenseVector(v)
-    new_array = list([float(x) for x in v])
-    return new_array
+# Function Definitions
+# sparseToDenseConvert() Method to convert sparse vector into a dense vector
+def sparseToDenseConvert(vect):
+    vect = DenseVector(vect)
+    newVector = list([float(x) for x in vect])
+    return newVector
 
-# returns one-hot-encoded stages
-def ohe(df):
-    # One-hot-encode category columns
-    categoricalColumns = ['education','currentSmoker', 'BPMeds', 'prevalentStroke', 'prevalentHyp', 'diabetes']
+# oneHotEncodeStages() Method returns a dataframe consisting of the categorical Columns selected
+def oneHotEncodeStages(dataFrame):
+    categoricalCols = ['education','currentSmoker', 'BPMeds', 'prevalentStroke', 'prevalentHyp', 'diabetes']
     stages = []
-    for categoricalCol in categoricalColumns:
-        stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
-        encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
-        stages += [stringIndexer, encoder]
+    for categCol in categoricalCols:
+        stringIndx = StringIndexer(inputCol = categCol, outputCol = categCol + 'Index')
+        encoder = OneHotEncoderEstimator(inputCols=[stringIndx.getOutputCol()], outputCols=[categCol + "classVec"])
+        stages += [stringIndx, encoder]
 
-    # vectorize numeric columns
-    numericCols = ['male', 'age', 'cigsPerDay', 'totChol', 'sysBP', 'diaBP', 'BMI', 'heartRate', 'glucose']
-    assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
-    assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+    numCols = ['male', 'age', 'cigsPerDay', 'totChol', 'sysBP', 'diaBP', 'BMI', 'heartRate', 'glucose']
+    assembInput = [c + "classVec" for c in categoricalCols] + numCols
+    assembler = VectorAssembler(inputCols=assembInput, outputCol="features")
     stages += [assembler]
-
-    # perform final pipeline to get vectorized feature column
     pipeline = Pipeline().setStages(stages)
-    pipelineModel = pipeline.fit(df)
-    df = pipelineModel.transform(df)
+    pipelineModel = pipeline.fit(dataFrame)
+    dataFrame = pipelineModel.transform(dataFrame)
     selectedCols = ['TenYearCHD', 'features'] + cols
-    df = df.select(selectedCols)
+    dataFrame = dataFrame.select(selectedCols)
+    sparseToDenseUDF = udf(sparseToDenseConvert, T.ArrayType(T.FloatType()))
+    dataFrame = dataFrame.withColumn('dense_vector_features', sparseToDenseUDF('features'))
+    udF = udf(lambda r : Vectors.dense(r), VectorUDT())
+    dataFrame = dataFrame.withColumn('features', udF('dense_vector_features'))
+    return dataFrame
 
-    # convert sparse vector initial_feature column to dense vector
-    sparse_to_array_udf = udf(sparse_to_array, T.ArrayType(T.FloatType()))
-    df = df.withColumn('dense_vector_features', sparse_to_array_udf('features'))
-    ud_f = udf(lambda r : Vectors.dense(r), VectorUDT())
-    df = df.withColumn('features', ud_f('dense_vector_features'))
-    return df
-
-def evaluation(TP, TN, FP, FN):
-    print('\n\n', 'The confusion matrix is: \n\n', '[[',TN,FP,']\n\n','[',FN,TP,']]')
-    #Model Evaluation - Statistics
-    sensitivity=TP/float(TP+FN)
-    specificity=TN/float(TN+FP)
-
-    print('\n\nThe acuuracy of the model = (TP+TN)/(TP+TN+FP+FN) = ',(TP+TN)/float(TP+TN+FP+FN),'\n\n',
-
-    'The Missclassification = 1-Accuracy = ',1-((TP+TN)/float(TP+TN+FP+FN)),'\n\n',
-
-    'Sensitivity or True Positive Rate = TP/(TP+FN) = ',TP/float(TP+FN),'\n\n',
-
-    'Specificity or True Negative Rate = TN/(TN+FP) = ',TN/float(TN+FP),'\n\n',
-
-    'Positive Predictive value = TP/(TP+FP) = ',TP/float(TP+FP),'\n\n',
-
-    'Negative predictive Value = TN/(TN+FN) = ',TN/float(TN+FN),'\n\n',
-
-    'Positive Likelihood Ratio = Sensitivity/(1-Specificity) = ',sensitivity/(1-specificity),'\n\n',
-
-    'Negative likelihood Ratio = (1-Sensitivity)/Specificity = ',(1-sensitivity)/specificity,'\n\n')
+# evaluate() Method Evaluates Confusion Matrix for The DataSets
+def evaluate(TP, TN, FP, FN):
+    print('\n\n', 'Confusion Matrix: \n\n', '[[', TN, FP, ']\n\n', '[', FN, TP, ']]')
+    sensitivity = TP/float(TP+FN)
+    specificity = TN/float(TN+FP)
+    print('\n\nAccuracy of the Model = (TP+TN)/(TP+TN+FP+FN) = ', (TP+TN)/float(TP+TN+FP+FN), '\n\n',
+    'Miss-classification = 1-Accuracy = ', 1-((TP+TN)/float(TP+TN+FP+FN)), '\n\n',
+    'True Positive Rate = TP/(TP+FN) = ', TP/float(TP+FN),'\n\n',
+    'True Negative Rate = TN/(TN+FP) = ', TN/float(TN+FP),'\n\n',
+    'Positive Predictive Value = TP/(TP+FP) = ', TP/float(TP+FP),'\n\n',
+    'Negative Predictive Value = TN/(TN+FN) = ', TN/float(TN+FN),'\n\n',
+    'Positive Likelihood Ratio = Positive Rate/(1-Negative Rate) = ', sensitivity/(1-specificity), '\n\n',
+    'Negative likelihood Ratio = (1-Positive Rate)/Negative Rate = ', (1-sensitivity)/specificity, '\n\n')
 
 
+### Main Program ###
 reload(sys)
 sys.setdefaultencoding('utf8')
-if __name__: # == "__main__":
-    # if len(sys.argv) != 2:
-        # print("Usage: Q2.py <directory_of_files>", file=sys.stderr)
-        # sys.exit(-1)
-        
+if __name__:
     spark = SparkSession.builder.appName('lr-predic').getOrCreate()
-    df = spark.read.csv(sys.argv[1], header = True, inferSchema = True)
-    df.show()
-    cols=df.columns
-    df.printSchema()
+    dataFrame = spark.read.csv(sys.argv[1], header = True, inferSchema = True)
+    dataFrame.show()
+    cols = dataFrame.columns
+    dataFrame.printSchema()
 
-    #Filter out all the 'NA' values
-    df = df.filter((df.cigsPerDay != 'NA')& (df.BPMeds != 'NA') & (df.totChol != 'NA') & (df.BMI != 'NA') & (df.heartRate != 'NA') & (df.glucose != 'NA'))
-    string_cols=['cigsPerDay', 'BPMeds','totChol','BMI', 'heartRate', 'glucose']
-    for col_name in string_cols:
-        df = df.withColumn(col_name, col(col_name).cast('float'))
+    # Ignore all 'NA' Values
+    dataFrame = dataFrame.filter((dataFrame.cigsPerDay != 'NA')& (dataFrame.BPMeds != 'NA') & (dataFrame.totChol != 'NA') & (dataFrame.BMI != 'NA') & (dataFrame.heartRate != 'NA') & (dataFrame.glucose != 'NA'))
+    stringColumns=['cigsPerDay', 'BPMeds','totChol','BMI', 'heartRate', 'glucose']
+    for col_name in stringColumns:
+        dataFrame = dataFrame.withColumn(col_name, col(col_name).cast('float'))
     
-    #Count of the class values
-    df.groupby('TenYearCHD').count().show()
-
-    #Drop the column of 'education'
-    #Generative statistics conclusion
+    dataFrame.groupby('TenYearCHD').count().show()
     cols.remove('education')
     cols.insert(0, 'Summary')
-    df.describe().select(cols[:len(cols)//2]).show()
-    df.describe().select(cols[len(cols)//2:-1]).show()
-
-    #Use pipeline to combine all the features in one single feature vector
+    dataFrame.describe().select(cols[:len(cols)//2]).show()
+    dataFrame.describe().select(cols[len(cols)//2:-1]).show()
     cols.remove('TenYearCHD')
     cols.remove('Summary')
     print(cols)
-
-    #['male', 'age', 'currentSmoker', 'cigsPerDay', 'BPMeds', 'prevalentStroke', 'prevalentHyp', 
-    # 'diabetes', 'totChol', 'sysBP', 'diaBP', 'BMI', 'heartRate', 'glucose']
-
-    df = ohe(df)
-    df.printSchema()
+    dataFrame = oneHotEncodeStages(dataFrame)
+    dataFrame.printSchema()
     
-    #Standarization
-    standardscaler=StandardScaler().setInputCol("features").setOutputCol("Scaled_features")
-    df=standardscaler.fit(df).transform(df)
-    df.select("features","Scaled_features").show(5)
+    # Standarize DataFrame
+    standardscaler = StandardScaler().setInputCol("features").setOutputCol("Scaled_features")
+    dataFrame = standardscaler.fit(dataFrame).transform(dataFrame)
+    dataFrame.select("features", "Scaled_features").show(5)
     
-    #Split train & test
-    train, test = df.randomSplit([0.8, 0.2], seed=12345)
+    train, test = dataFrame.randomSplit([0.8, 0.2], seed=12345)
 
-    total=float(train.select("TenYearCHD").count())
-    numPositives=train.select("TenYearCHD").where('TenYearCHD == 1').count()
-    per_ones=(float(numPositives)/float(total))*100
-    numNegatives=float(total-numPositives)
-    print('\n\nThe number of Class 1 are {}'.format(numPositives))
-    print('\n\nPercentage of Class 1 are {}'.format(per_ones))
+    total = float(train.select("TenYearCHD").count())
+    positiveNum = train.select("TenYearCHD").where('TenYearCHD == 1').count()
+    pOnes = (float(positiveNum)/float(total))*100
+    negativeNum = float(total-positiveNum)
+    print('\n\nThe number of Class 1 are {}'.format(positiveNum))
+    print('\n\nPercentage of Class 1 are {}'.format(pOnes))
 
-    #Add balancing ratio
-    BalancingRatio= numNegatives/total
+    BalancingRatio= negativeNum/total
     print('\n\nBalancingRatio = {}'.format(BalancingRatio))
+    train = train.withColumn("classWeights", when(train.TenYearCHD==1, BalancingRatio).otherwise(1-BalancingRatio))
+    train.select("classWeights", "TenYearCHD").show(5)
     
-    #Creat a new column named “classWeights” in the “train” dataset
-    train=train.withColumn("classWeights", when(train.TenYearCHD==1, BalancingRatio).otherwise(1-BalancingRatio))
-    train.select("classWeights", "TenYearCHD").show(10)
-    
-    #Feature selection
-    css = ChiSqSelector(featuresCol='features',outputCol='Aspect',labelCol='TenYearCHD',fpr=0.05)
-    train=css.fit(train).transform(train)
-    test=css.fit(test).transform(test)
-    test.select("Aspect").show(5,truncate=False)
+    # Feature selection
+    css = ChiSqSelector(featuresCol = 'features', outputCol='Aspect', labelCol='TenYearCHD', fpr = 0.05)
+    train = css.fit(train).transform(train)
+    test = css.fit(test).transform(test)
+    test.select("Aspect").show(5, truncate = False)
 
-    #Train the lr model
-    lr = LogisticRegression(labelCol="TenYearCHD", featuresCol="Aspect",weightCol="classWeights", maxIter=10)
-    model=lr.fit(train)
-
-    #Printing the coefficients and intercept for logistic regression
-    #lr_coeff=model.coefficients
-    #print('\n\nCoefficients: ', [round(i, 3) for i in lr_coeff],'\n\n')
-
-    predict_train=model.transform(train)
-    predict_test=model.transform(test)
-    predict_train.select("TenYearCHD","prediction").show(10)
-    predict_test.select("TenYearCHD","prediction").show(10) 
-    
-    #Evaluation of the train model
+    # Training Model
+    lr = LogisticRegression(labelCol="TenYearCHD", featuresCol="Aspect", weightCol="classWeights", maxIter=10)
+    model = lr.fit(train)
+    predict_train = model.transform(train)
+    predict_test = model.transform(test)
+    predict_train.select("TenYearCHD","prediction").show(5)
+    predict_test.select("TenYearCHD","prediction").show(5) 
     TP_train = predict_train.filter((predict_train.TenYearCHD == 1) & (predict_train.prediction == 1.0)).count()
     TN_train = predict_train.filter((predict_train.TenYearCHD == 0) & (predict_train.prediction == 0.0)).count()
     FP_train = predict_train.filter((predict_train.TenYearCHD == 0) & (predict_train.prediction == 1.0)).count()
     FN_train = predict_train.filter((predict_train.TenYearCHD == 1) & (predict_train.prediction == 0.0)).count()    
     print('\n\nEvaluation Results for Training Dataset')
-    evaluation(TP_train, TN_train, FP_train, FN_train)
+    evaluate(TP_train, TN_train, FP_train, FN_train)
     
-    #Evaluation of the test model
+    # Testing Model
     TP_test = predict_test.filter((predict_test.TenYearCHD == 1) & (predict_test.prediction == 1.0)).count()
     TN_test = predict_test.filter((predict_test.TenYearCHD == 0) & (predict_test.prediction == 0.0)).count()
     FP_test = predict_test.filter((predict_test.TenYearCHD == 0) & (predict_test.prediction == 1.0)).count()
     FN_test = predict_test.filter((predict_test.TenYearCHD == 1) & (predict_test.prediction == 0.0)).count()
     print('\n\nEvaluation Results for Test Dataset')
-    evaluation(TP_test, TN_test, FP_test, FN_test)
+    evaluate(TP_test, TN_test, FP_test, FN_test)
 
-    
-    #ROC for train & test
-    evaluator=BinaryClassificationEvaluator(rawPredictionCol="rawPrediction",labelCol="TenYearCHD")
-    predict_test.select("TenYearCHD","rawPrediction","prediction","probability").show(5, truncate = False)
+    # ROC for Training and Test DataSets
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol = "rawPrediction", labelCol = "TenYearCHD")
+    predict_test.select("TenYearCHD", "rawPrediction", "prediction", "probability").show(5, truncate = False)
 
     print("\n\nThe area under ROC for train set is {}".format(evaluator.evaluate(predict_train)))
     print("\n\nThe area under ROC for test set is {}".format(evaluator.evaluate(predict_test)))
